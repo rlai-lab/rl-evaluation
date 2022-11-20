f46f01a7ef8c095aa6baab8f34d95716df0fcd30
andy <andnpatterson@gmail.com>
2022-11-20 15:04:57 -0700

    chore: setup basic repo structure

---
+++ typings/numba/__init__.pyi
@@ -0,0 +1,13 @@
+# from typing import Callable
+from typing import Any, Callable, overload, TypeVar
+
+T = TypeVar('T')
+
+@overload
+def njit(cache: bool = False, parallel: bool = False, nogil: bool = False, fastmath: bool = False) -> Callable[[T], T]: ...
+@overload
+def njit(f: T, cache: bool = False, parallel: bool = False, nogil: bool = False, fastmath: bool = False) -> T: ...
+
+def jit(cache: bool, forceobj: bool) -> Callable[[T], T]: ...
+
+def vectorize(f: Any, cache: bool) -> Any: ...
