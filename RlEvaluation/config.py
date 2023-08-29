32395bddc0ee96112355058a81bfb0d481860652
andy <andnpatterson@gmail.com>
2023-08-29 16:11:33 -0600

    feat: make configuration a global dataclass

---
+++ RlEvaluation/config.py
@@ -0,0 +1,50 @@
+import dataclasses
+
+from typing import Iterable, List
+
+@dataclasses.dataclass
+class DataDefinition:
+    hyper_cols: List[str]
+    seed_col: str
+    time_col: str
+    environment_col: str
+    algorithm_col: str
+
+
+_data_def: DataDefinition | None = None
+
+def data_definition(
+    hyper_cols: Iterable[str],
+    seed_col: str = 'seed',
+    time_col: str = 'frame',
+    environment_col: str = 'environment',
+    algorithm_col: str = 'algorithm',
+
+    make_global: bool = False,
+) -> DataDefinition:
+    d = DataDefinition(
+        hyper_cols=list(sorted(hyper_cols)),
+        seed_col=seed_col,
+        time_col=time_col,
+        environment_col=environment_col,
+        algorithm_col=algorithm_col,
+    )
+
+    if make_global:
+        global _data_def
+        _data_def = d
+
+    return d
+
+
+def get_global_data_def():
+    global _data_def
+    assert _data_def is not None
+    return _data_def
+
+
+def maybe_global(d: DataDefinition | None):
+    if d is None:
+        return get_global_data_def()
+
+    return d
