18304ad8e71d50334ac2a1aa711fefb457ba9566
andy <andnpatterson@gmail.com>
2023-09-05 10:49:22 -0600

    fix: add type-check before isnan

--- RlEvaluation/hypers/reporting.py
+++ RlEvaluation/hypers/reporting.py
@@ -21,20 +21,22 @@ def pretty_print_hyper_selection_result(result: HyperSelectionResult, d: DataDef
     out += 'Best configuration setting:\n'
     out += '---------------------------\n'
     for hyper, value in zip(d.hyper_cols, result.best_configuration):
-        if np.isnan(value): continue
+        if isinstance(value, float) and np.isnan(value): continue
         ws = 4 + col_len - len(hyper)
         out += f'{hyper}:' + ' ' * ws
         out += f'{value}\n'
 
     out += '\n'
     out += f'Score for best setting: {result.best_score:.3f}\n'
+    out += f'Confidence interval: ({result.ci[0]:.3f}, {result.ci[1]:.3f})\n'
     out += '---------------------------\n\n'
 
     if len(result.uncertainty_set_probs) > 1:
         out += 'Possible best configurations:\n'
         out += '-----------------------------\n'
         for i, hyper in enumerate(d.hyper_cols):
-            if np.isnan(result.uncertainty_set_configurations[0][i]): continue
+            hyper_val = result.uncertainty_set_configurations[0][i]
+            if isinstance(hyper_val, float) and np.isnan(hyper_val): continue
             ws = 4 + col_len - len(hyper)
             out += f'{hyper}:' + ' ' * ws
 
@@ -46,6 +48,5 @@ def pretty_print_hyper_selection_result(result: HyperSelectionResult, d: DataDef
         out += '\n'
         out += f'With probabilities: {result.uncertainty_set_probs}\n'
         out += f'Score over best configurations: {result.sample_stat:.3f}\n'
-        out += f'Confidence interval: ({result.ci[0]:.3f}, {result.ci[1]:.3f})'
 
     print(out)
