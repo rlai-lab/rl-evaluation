b1a597f9cc0d2a381bd28612ac0bf448fbc1c617
andy <andnpatterson@gmail.com>
2023-09-12 15:02:57 -0600

    fix: use episode length for weighting

--- RlEvaluation/metrics.py
+++ RlEvaluation/metrics.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 from RlEvaluation.config import DataDefinition, maybe_global
 
-def add_step_weighted_return(df: pd.DataFrame, return_col: str = 'return', d: DataDefinition | None = None):
+def add_step_weighted_return(
+    df: pd.DataFrame,
+    episode_len_col: str = 'steps',
+    return_col: str = 'return',
+    d: DataDefinition | None = None,
+):
     d = maybe_global(d)
     cols = _get_data_def_columns(df, d)
 
     def red(sub):
-        return sub[return_col] * (sub[d.time_col] / sub[d.time_col].max())
+        return sub[return_col] * (sub[episode_len_col] / sub[d.time_col].max())
 
     groups = df.groupby(cols, dropna=False, as_index=False, group_keys=False)
     df['step_weighted_return'] = groups.apply(red)
