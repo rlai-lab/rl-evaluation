42b45d8ba4bd5943542622eef74fbc7d812c785c
andy <andnpatterson@gmail.com>
2023-09-20 14:46:11 -0600

    fix: harden interpolation assumptions (such as monotonicity of time)

--- RlEvaluation/interpolation.py
+++ RlEvaluation/interpolation.py
@@ -12,18 +12,17 @@ Interpolation = Callable[[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]
 @nbu.njit
 def compute_step_return(t: np.ndarray, m: np.ndarray, max_length: int) -> Tuple[np.ndarray, np.ndarray]:
     assert t.shape == m.shape
+    assert np.all(t[1:] > t[:-1]), 'Time column should be strictly increasing'
+
     x = np.arange(max_length)
     out = np.empty(max_length, dtype=m.dtype)
 
-    acc = 0
+    last = 0
     for i in range(t.shape[0]):
-        steps = t[i]
-        n = acc + steps
+        out[last:t[i]] = m[i]
+        last = t[i]
 
-        out[acc:n] = m[i]
-        acc = n
-
-    out[acc:] = m[-1]
+    out[t[-1]:] = m[-1]
 
     return x, out
 
