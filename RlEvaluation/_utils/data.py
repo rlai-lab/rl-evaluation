b3fe5a744c7e39572667615c3ef64583f52690fc
Andy Patterson <andnpatterson@gmail.com>
2024-09-04 10:17:55 -0600

    style: fix ambiguous naming

--- RlEvaluation/_utils/data.py
+++ RlEvaluation/_utils/data.py
@@ -33,14 +33,14 @@ def compileReducer(f: Reducer):
     # normalize return type of the reducer
     return try2jit_no_cache(lambda arr: float(f(arr)))
 
-def ragged_to_square(l: Sequence[np.ndarray] | np.ndarray) -> np.ndarray:
-    if np.isscalar(l[0]):
-        return np.expand_dims(l, axis=1)
+def ragged_to_square(seq: Sequence[np.ndarray] | np.ndarray) -> np.ndarray:
+    if np.isscalar(seq[0]):
+        return np.expand_dims(seq, axis=1)
 
-    m = max(len(x) for x in l)
-    out = np.empty((len(l), m), dtype=l[0].dtype)
+    m = max(len(x) for x in seq)
+    out = np.empty((len(seq), m), dtype=seq[0].dtype)
 
-    for i, x in enumerate(l):
+    for i, x in enumerate(seq):
         assert x.ndim == 1
 
         needed = m - len(x)
