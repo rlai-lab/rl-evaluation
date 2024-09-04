e1665e5c49227946a9f175d57e3fb0856c2bc22e
Andy Patterson <andnpatterson@gmail.com>
2024-09-04 12:08:39 -0600

    test: switch to pytest

--- tests/test_hypers.py
+++ tests/test_hypers.py
@@ -1,18 +1,16 @@
-import unittest
 import pandas as pd
 
 from rlevaluation.hypers import select_best_hypers, Preference
 from rlevaluation.config import data_definition
 
-class TestHypers(unittest.TestCase):
-    def test_select_best_hypers(self):
-        test_data = pd.DataFrame({
-            'alpha': [0.1, 0.01, 0.001],
-            'seed': [0, 0, 0],
-            'result': [0, 2, 1],
-        })
+def test_select_best_hypers():
+    test_data = pd.DataFrame({
+        'alpha': [0.1, 0.01, 0.001],
+        'seed': [0, 0, 0],
+        'result': [0, 2, 1],
+    })
 
-        d = data_definition(hyper_cols=['alpha'])
+    d = data_definition(hyper_cols=['alpha'])
 
-        best = select_best_hypers(test_data, 'result', Preference.high, data_definition=d)
-        self.assertEqual(best.best_configuration[0], 0.01)
+    best = select_best_hypers(test_data, 'result', Preference.high, data_definition=d)
+    assert best.best_configuration[0] == 0.01
