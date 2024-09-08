ccd28eac191ba935d61ac7c8d4a067c02d47e54f
andnp <andnpatterson@gmail.com>
2024-09-08 10:23:37 -0600

    refactor: complete transition to polars over pandas

--- rlevaluation/_utils/data.py
+++ rlevaluation/_utils/data.py
@@ -1,33 +1,9 @@
 import numpy as np
-import pandas as pd
-from typing import Callable, Sequence, Set, Union
+from typing import Callable, Sequence, Union
 from rlevaluation._utils.jit import try2jit_no_cache
 
-Data = Union[np.ndarray, pd.DataFrame, pd.Series]
 Numeric = Union[np.ndarray, float]
 Reducer = Callable[[np.ndarray], Numeric]
-
-def normalizeDataType(data: Data, dims: int, col: str = '') -> np.ndarray:
-    if isinstance(data, pd.DataFrame):
-        assert col != ''
-        d = data[col].to_numpy()
-        data = ragged_to_square(d)
-
-    elif isinstance(data, pd.Series):
-        assert col != ''
-        data = np.asarray(data[col])
-
-    if np.ndim(data) > dims:
-        raise Exception('Data has too many dimensions')
-
-    elif np.ndim(data) < dims:
-        needed = dims - np.ndim(data)
-        shape = data.shape + tuple(1 for _ in range(needed))
-        data = data.reshape(shape)
-
-    assert np.ndim(data) == dims
-    assert isinstance(data, np.ndarray)
-    return data
 
 def compileReducer(f: Reducer):
     # normalize return type of the reducer
@@ -51,28 +27,3 @@ def ragged_to_square(seq: Sequence[np.ndarray] | np.ndarray) -> np.ndarray:
             out[i] = x
 
     return out
-
-def get_other_cols(df: pd.DataFrame, known: Set[str], seed: str):
-    cols = set(df.columns)
-    return cols - (known | {seed})
-
-def is_wide_format(df: pd.DataFrame, metrics: Set[str], seed_col: str):
-    if seed_col in df:
-        return False
-
-    if any(not isinstance(df[m].iloc[0], np.ndarray) for m in metrics):
-        return False
-
-    return True
-
-def make_wide_format(df: pd.DataFrame, hypers: Set[str], metrics: Set[str], seed_col: str):
-    if is_wide_format(df, metrics, seed_col):
-        return df
-
-    op = {
-        c: lambda x: np.array(list(x))
-        for c in metrics
-    }
-
-    df = df.groupby(list(hypers)).agg(op).reset_index()
-    return df
--- tests/test_utils/mock_data.py
+++ tests/test_utils/mock_data.py
@@ -1,33 +1,8 @@
 from typing import Any, Dict
 import numpy as np
-import pandas as pd
 import polars as pl
 
 from itertools import product
-
-def generate_wide_data():
-    df = pd.DataFrame(columns=['stepsize', 'optimizer', 'results'])
-
-    alphas = [0.1, 0.01, 0.001]
-    opts = ['adam', 'rmsprop']
-    for i, (a, opt) in enumerate(product(alphas, opts)):
-        r = np.random.randn(10, 300)
-        df.loc[i] = [a, opt, r]
-
-    return df
-
-def generate_split_over_seed():
-    df = pd.DataFrame(columns=['stepsize', 'optimizer', 'run', 'results'])
-
-    alphas = [0.1, 0.01, 0.001]
-    opts = ['adam', 'rmsprop']
-    runs = range(10)
-    for i, (a, opt, r) in enumerate(product(alphas, opts, runs)):
-        res = np.random.randn(300)
-        df.loc[i] = [a, opt, r, res]
-
-    return df
-
 
 def learning_curve_data():
     hypers = {
